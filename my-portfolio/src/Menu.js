a3d7262eb601a763d05ecbc1d91a026f0e6d295c
Kielan2 <kielananderson@gmail.com>
2022-08-23 16:11:20 -0600

    menu changes

--- my-portfolio/src/Menu.js
+++ my-portfolio/src/Menu.js
@@ -8,19 +8,28 @@ function Menu() {
   return (
       <div className='menu'>
         <div className="border">
+        <div>
         <h1>Get in touch</h1>
         <span>Reach out and let's start a conversation!</span>
+        </div>
+        <ul className="skills">
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>JavaScript</li>
+            <li>MongoDB</li>
+            <li>Express</li>
+            <li>ReactJS</li>
+            <li>NodeJS</li>
+            <li>SEO</li>
+            <li>UX/UI</li>
+        </ul>
         
         <ul className="iconList">
-            <li><a href='https://www.linkedin.com/in/kielan-anderson/'><LinkedInIcon fontSize='large' /></a>LinkedIn</li>
-            <li><a href='https://github.com/Kielan2'><GitHubIcon fontSize='large' /></a>Github</li>
-            <li><a href='mailto:kielananderson@gmail.com'><EmailIcon fontSize='large' /></a>Email</li>
+            <li><a href='https://www.linkedin.com/in/kielan-anderson/'><LinkedInIcon fontSize='large' /></a></li>
+            <li><a href='https://github.com/Kielan2'><GitHubIcon fontSize='large' /></a></li>
+            <li><a href='mailto:kielananderson@gmail.com'><EmailIcon fontSize='large' /></a></li>
         </ul>
-        <ul className='infoList'>
-        {/* <li>Based in: Salt Lake City, UT<br />(Open to Relocation)</li>
-        {/* <li className='hover'><a href='https://www.linkedin.com/in/kielan-anderson/'>kielananderson@gmail.com</a></li>
-        <li className='hover'><a href='tel:+13852269399'>(385) 226-9399</a></li> */} 
-        </ul>
+        
         
         </div>
     </div>
