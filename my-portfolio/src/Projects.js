caa5dcb7b1efe9bd5180ada0b072422a42c49c3d
Kielan2 <kielananderson@gmail.com>
2022-09-10 14:44:32 -0600

    couple transitions

--- my-portfolio/src/Projects.js
+++ my-portfolio/src/Projects.js
@@ -7,17 +7,17 @@ function Projects() {
             <h2>Latest Projects</h2>
         <div className="project">
             <img src="/netflixclonescreenshot.png" alt="Netflix Clone"/>
-            <h3><a href='https://kielanflix.netlify.app/'>Netflix Clone ></a></h3>
+            <h3><a href='https://kielanflix.netlify.app/'>Netflix Clone</a></h3>
         </div>
         
         <div className="project">
-            <h3><a href='https://kielanapi.netlify.app/'>Star Wars ></a></h3>
+            <h3><a href='https://kielanapi.netlify.app/'>Star Wars</a></h3>
             <img src="/starWarsProject.png" alt="Star Wars API Project"/>
         </div>
 
         <div className="project">
             <img src="/musicPlayer.png" alt="Music Player"/>
-            <h3><a href='https://kielanflix.netlify.app/'>Music Player ></a></h3>
+            <h3><a href='https://kielanflix.netlify.app/'>Music Player</a></h3>
         </div>
         <footer>
             <span>Kielan Anderson | 2022</span>
